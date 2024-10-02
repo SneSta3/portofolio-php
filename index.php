bb04cbfdbf8a305b4e8d8caecdcfcc622740d3f0
SneSta3 <snjezastakic@gmail.com>
2024-10-02 10:55:57 +0200

    header i posts

--- index.php
+++ index.php
@@ -15,6 +15,39 @@
         </ul>
     </nav>
 
+    <header class="container">
+        <div>
+            <h1>Hi, I am John, <br> Creative Technologist</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam voluptatem, deleniti, fuga, doloribus modi facilis quas vitae iusto error et eligendi sapiente labore deserunt dignissimos provident necessitatibus repellendus animi adipisci?</p>
+            <button class="button">Download Resume</button>
+        </div>
+        <div>
+            <img src="img/header.png" alt="">
+        </div>
+    </header>
+
+    <div class="recent-posts">
+        <div class="container">
+            <div class="row">
+                <p>Resents Posts</p>
+                <a href="">View all</a>
+            </div>
+            <div class="row">
+                <div class="post">
+                <h3>Making a desing system from scratch</h3>
+                <p>12 Feb 2020 &nbsp; | &nbsp; Design, Paterns</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident fugiat accusamus omnis veniam magnam? Praesentium possimus placeat soluta optio natus?</p>
+                </div>
+                <div class="post">
+                <h3>Creating pixel perfect icons in Figma</h3>
+                <p>12 Feb 2020 &nbsp; | &nbsp; Figma, Icon design</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident fugiat accusamus omnis veniam magnam? Praesentium possimus placeat soluta optio natus?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     
     
 </body>
